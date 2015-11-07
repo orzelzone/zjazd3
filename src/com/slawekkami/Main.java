9cba4a49ab772bb7f77d90811b8c6db67c8d4060
imie <y@y.com>
2015-11-07 14:05:38 +0100

    pierwsze

---
+++ src/com/slawekkami/Main.java
@@ -0,0 +1,15 @@
+package com.slawekkami;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int liczba = 50;
+        int nieparzysta = 1;
+        while(nieparzysta<liczba) {
+            System.out.print(nieparzysta + ", ");
+            nieparzysta = nieparzysta + 2;
+        }
+    }
+}
+
